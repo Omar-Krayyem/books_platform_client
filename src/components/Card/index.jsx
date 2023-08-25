b79b14f149667917c9607d1118f01db4c4e6032b
Omar-Krayyem <omar.krayyem.b@gmail.com>
2023-08-25 03:07:26 +0300

    Client <react> fetched like and unlike apis

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,7 +1,7 @@
 import '../Card/style.css';
 // import FavoriteIcon from '@mui/icons-material/Favorite';
 // import ThumbUpAltRoundedIcon from '@mui/icons-material/ThumbUpAltRounded';4
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { AiFillLike } from 'react-icons/ai';
 import axios from 'axios';
 
@@ -12,36 +12,18 @@ const Card = (props) => {
 
     /////////////////////////////////////////////////////////////////////////////FOLLOW
 
-    let [isFollowed, setIsFollowed] = useState(false);
+    let [isFollowed, setIsFollowed] = useState();
 
     setIsFollowed = props.isFollowing;
 
 
     /////////////////////////////////////////////////////////////////////////////LIKE
-    let [like, setLike] = useState('0');
-    let [isLiked, setIsLiked] = useState(false);
-    setLike = props.likeCount;
-
-    
-
-    const getLikes = async () => {
-        await axios.get(`http://127.0.0.1:8000/api/like/count/${post_id}`, {
-            "headers": {
-                'Authorization': `Bearer ${token}`
-            }
-        })
-        .then(response => {
-            // console.log(response.data.data);
-            setLike(response.data.data);
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    };
+    let [like, setLike] = useState(props.likeCount);
+    let [isLiked, setIsLiked] = useState(props.isLiked);
 
     const handleLikeBtn = () => {
         if (isLiked) {
-            axios.delete(`http://127.0.0.1:8000/api/like/destroy/${post_id}`, {
+            axios.post(`http://127.0.0.1:5000/books/${post_id}/unlike`, null, {
                 headers: {
                     'Authorization': `Bearer ${token}`
                 }
@@ -49,22 +31,23 @@ const Card = (props) => {
             .then(response => {
                 // console.log(response.data)
                 setIsLiked(false);
-                getLikes();
+                setLike(like - 1);
             })
             .catch(error => {
                 console.log(error);
             });
         } else {
-            const postData = { post_id};
-            axios.post(`http://127.0.0.1:8000/api/like/store`, postData, {
+            // const postData = { post_id};
+            axios.post(`http://127.0.0.1:5000/books/${post_id}/like`, null, {
                 headers: {
                     'Authorization': `Bearer ${token}`
                 }
             })
             .then(response => {
-                // console.log(response.data)
+                console.log(response.data)
                 setIsLiked(true);
-                getLikes();
+                setLike(like + 1)
+                console.log(like)
             })
             .catch(error => {
                 console.log(error);
@@ -86,6 +69,7 @@ const Card = (props) => {
                         <p>by {props.username}</p>
                     </div>
                     <div className="card_details">
+                            <h4>Author: {props.author}</h4>
                             <h4>Genre: {props.genre}</h4>
                             <h4>Review:</h4>
                             <p>{props.review}</p>
@@ -93,7 +77,6 @@ const Card = (props) => {
                 </div>
                 <div className="below">
                     <h3 >{isFollowed ? 'unfollow' : 'follow'}</h3>
-                    {/* <div  className={`likes ${isLiked ? 'liked' : ''}`}>{like}<FavoriteIcon className={`icon ${isLiked?'liked':''}`} onClick={handleLikeBtn}/></div> */}
                     <div className='likes'> {like} <AiFillLike className={`like_icon ${isLiked ? 'liked' : ''}`} onClick={handleLikeBtn}/></div>
                 </div>
             </div>  
